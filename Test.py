3e990ced424b29723a64586068b9e7c81d344a75
Sadi Çetinkaya <cetinkaya.sadi@gmail.com>
2018-12-18 15:07:31 +0300

    Main Test file

---
+++ Test.py
@@ -0,0 +1,25 @@
+from sklearn import datasets
+import pandas as pd
+from Descriptive import *
+
+boston = datasets.load_boston()
+
+b = pd.DataFrame(boston.data,columns = ['CRIM','ZN','INDUS','CHAS','NOX','RM','AGE','DIS','RAD','TAX','PTRATIO','B','LSTAT'])
+
+main = stats(b)
+info = main.info()
+stats = main.desc_stats()
+freq = main.frequency(3)
+#inter = main.interval(0,5)
+
+cf = pd.value_counts(b.ix[10:15, 1]).to_frame().reset_index()
+ef = b.ix[10:13, 1].shape[0]
+
+print(b.head() , "\n")
+print(info , "\n")
+
+print(stats , "\n")
+
+print(freq , "\n")
+
+print(ef , "\n")
